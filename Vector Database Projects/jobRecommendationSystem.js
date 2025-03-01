b30e6cec9df2690916425e32b6d95c93b76f417e
Dennis Lam <39684447+dennislamcv1@users.noreply.github.com>
2025-03-01 19:49:46 +0800

    Add files via upload

--- Vector Database Projects/jobRecommendationSystem.js
+++ Vector Database Projects/jobRecommendationSystem.js
@@ -1,139 +1,111 @@
+
 const { ChromaClient } = require("chromadb");
 const client = new ChromaClient();
 const { HfInference } = require("@huggingface/inference");
-const hf = new HfInference("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");
+
+const hf = new HfInference("xxx");
+const jobPostings = require('./jobPostings.js');
 const collectionName = "job_collection";
 
 async function generateEmbeddings(texts) {
-  const results = await hf.featureExtraction({
-    model: "sentence-transformers/all-MiniLM-L6-v2",
-    inputs: texts,
-  });
-  return results;
+    const results = await hf.featureExtraction({
+        model: "sentence-transformers/all-MiniLM-L6-v2",
+        inputs: texts,
+    });
+    return results;
 }
 
-async function classifyText(word, labels) {
-  const response = await hf.request({
-    model: "facebook/bart-large-mnli",
-    inputs: word,
-  });
-
-  if (!response || !response.labels || !response.scores) {
-    console.error("Invalid response from classification model");
-    return { labels: [], scores: [] };
-  }
-
-  return response;
+async function classifyText(text, labels) {
+    const response = await hf.request({
+        model: "facebook/bart-large-mnli",
+        inputs: text,
+        parameters: { candidate_labels: labels },
+    });
+    return response;
 }
 
-async function extractFilterCriteria(query) {
-  const criteria = { location: null, jobTitle: null, company: null, jobType: null };
-  const labels = ["location", "job title", "company", "job type"];
- 
-  const words = query.split(" ");
-  for (const word of words) {
-    const result = await classifyText(word, labels);
-    console.log('result', result);
+async function extractFilterCriteria (query) {
+    const criteria = { location: null, jobTitle: null, company: null, jobType: null };
+    const labels = ["location", "jobTitle", "company", "jobType"];
+   
+    const result = await classifyText(query,labels);
+
     const highestScoreLabel = result.labels[0];
     const score = result.scores[0];
- 
-    if (score > 0.5) {
-      switch (highestScoreLabel) {
-        case "location":
-          criteria.location = word;
-          break;
-        case "job title":
-          criteria.jobTitle = word;
-          break;
-        case "company":
-          criteria.company = word;
-          break;
-        case "job type":
-          criteria.jobType = word;
-          break;
-        default:
-          break;
-      }
+
+    if(score > 0.5){
+        criteria[`${highestScoreLabel}`] = result.sequence;
     }
-  }
-  console.log('Extracted Filter Criteria:', criteria);
-  return criteria;
+
+    return criteria;
 }
 
-async function performSimilaritySearch(collection, queryTerm, jobPostings) {
-  try {
-    const queryEmbedding = await generateEmbeddings([queryTerm]);
-    const results = await collection.query({
-      collection: collectionName,
-      queryEmbeddings: queryEmbedding,
-      n: 3,
-    });
+async function performSimilaritySearch(collection, queryTerm, filterCriteria) {
+    try {
+        const queryEmbedding = await generateEmbeddings([queryTerm]);
 
-    if (!results || results.length === 0) {
-      console.log(`No similar results found to "${queryTerm}"`);
-      return [];
+        const results = await collection.query({
+            collection: collectionName,
+            queryEmbeddings: queryEmbedding,
+            n: 3,
+        });
+
+        if (!results || results.length === 0) {
+            console.log(`No items found similar to "${queryTerm}"`);
+            return [];
+        }
+
+        let topJobPostings = results.ids[0].map((id, index) => {
+            const job = jobPostings.find(job => job.jobId.toString() === id);
+            return {
+                id,
+                score: results.distances[0][index],
+                ...job,
+            };
+        }).filter(Boolean);
+
+        return topJobPostings.sort((a, b) => a.score - b.score);
+    } catch (error) {
+        console.error("Error during similarity search:", error);
+        return [];
     }
-
-    let topJobPostings = results.ids[0].map((id, index) => {
-      return {
-        id,
-        score: results.distances[0][index],
-        jobTitle: jobPostings.find(item => item.job_id.toString() === id).jobTitle,
-        jobType: jobPostings.find(item => item.job_id.toString() === id).jobType,
-        jobDescription: jobPostings.find(item => item.job_id.toString() === id).jobDescription,
-        company: jobPostings.find(item => item.job_id.toString() === id).company
-      };
-    }).filter(Boolean);
-    return topJobPostings.sort((a, b) => b.score - a.score);
-  } catch (error) {
-    console.error("Error during similarity search:", error);
-    return [];
-  }
 }
 
 async function main() {
-  const query = "Creative Studio";
-  try {
-    const collection = await client.getOrCreateCollection({ name: collectionName });
-    
-    const uniqueIds = new Set();
-    jobPostings.forEach((job, index) => {
-      while (uniqueIds.has(job.job.toString())) {
-        job.job = `${job.job}_${index}`;
-      }
-      uniqueIds.add(job.job.toString());
-    });
+    const query = "Creative Studio";
 
-    const jobTexts = jobPostings.map((job) => `${job.jobTitle}. ${job.jobDescription}. ${job.jobType}. ${job.location}.`);
-    const embeddingsData = await generateEmbeddings(jobTexts);
-    
-    const metadatas = jobPostings.map((job) => ({
-      location: job.location,
-      jobTitle: job.jobTitle,
-      company: job.company,
-    }));
+    try {
+        const collection = await client.getOrCreateCollection({ name: collectionName });
+        const uniqueIds = new Set();
 
-    await collection.add({
-      ids: jobPostings.map((job) => job.jobTitle.toString()),
-      documents: jobTexts,
-      embeddings: embeddingsData,
-      metadatas: metadatas,
-    });
+        jobPostings.forEach((job, index) => {
+            while (uniqueIds.has(job.jobId.toString())) {
+                job.jobId = `${job.jobId}_${index}`;
+            }
+            uniqueIds.add(job.jobId.toString());
+        });
 
-    const filterCriteria = await extractFilterCriteria(query);
-    console.log('Filter Criteria:', filterCriteria);
-    
-    const initialResults = await performSimilaritySearch(collection, query, jobPostings);
-    initialResults.slice(0, 3).forEach((item, index) => {
-      console.log(`Top ${index + 1} Recommended Job Title: ${item.jobTitle}`);
-      console.log(`Top ${index + 1} Recommended Job Type: ${item.jobType}`);
-      console.log(`Top ${index + 1} Recommended Job Description: ${item.jobDescription}`);
-      console.log(`Top ${index + 1} Recommended Company: ${item.company}`);
-    });
-  } catch (error) {
-    console.error("Error:", error);
-  }
+        const jobTexts = jobPostings.map((job) => `jobTitle: ${job.jobTitle}. jobDescription: ${job.jobDescription}. jobType: ${job.jobType}. jobLocation: ${job.location}`);
+        const embeddingsData = await generateEmbeddings(jobTexts);
+
+        await collection.add({
+            ids: jobPostings.map((job) => job.jobId.toString()),
+            documents: jobTexts,
+            embeddings: embeddingsData,
+        });
+
+        const filterCriteria = await extractFilterCriteria(query);
+        console.log('Filter Criteria:', filterCriteria);
+
+        const initialResults = await performSimilaritySearch(collection, query, filterCriteria);
+
+        initialResults.slice(0, 3).forEach(({jobTitle, jobDescription, jobType, company}, index) => {
+            console.log(`Top ${index + 1} jobTitle: ${jobTitle}, jobType: ${jobType}, jobDescription: ${jobDescription}, Company: ${company}`);
+        });
+
+    } catch (error) {
+        console.error("Error:", error);
+    }
 }
 
-// Run the main function
 main();
--- Vector Database Projects/smartRecommendationSystem.js
+++ Vector Database Projects/smartRecommendationSystem.js
@@ -4,129 +4,113 @@ const { HfInference } = require("@huggingface/inference");
 const readline = require("readline");
 const { ChromaClient } = require("chromadb");
 
-const hf = new HfInference("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");
+const hf = new HfInference("xxx");
 
 const chroma = new ChromaClient();
-const collectionName = "job_postings";
+const collectionName = "job_collection";
+const jobPostings = require('./jobPostings.js');
 
-const smartRecommendationSystem = require('./jobPostings.js');
 
-// Function to extract text from a PDF file
 const extractTextFromPDF = async (filePath) => {
+  try {
+    const dataBuffer = fs.readFileSync(filePath);
+    const data = await pdf(dataBuffer);
+    const text = data.text.replace(/\n/g, " ").replace(/ +/g, " ");
+    return text;
+  } catch (err) {
+    console.error("Error extracting text from PDF:", err);
+    throw err;
+  }
+};
+
+
+const generateEmbeddings = async (text) => {
     try {
-      const dataBuffer = fs.readFileSync(filePath);
-      const data = await pdf(dataBuffer);
-      const text = data.text.replace(/\n/g, " ").replace(/ +/g, " ");
-      return text;
+      const result = await hf.featureExtraction({
+        model: "sentence-transformers/all-MiniLM-L6-v2",
+        inputs: text,
+      });
+
+  return result
     } catch (err) {
-      console.error("Error extracting text from PDF:", err);
+      console.error("Error converting text to embeddings:", err);
       throw err;
     }
   };
-  // Function to convert text to embeddings using Hugging Face embeddings
-  const generateEmbeddings = async (text) => {
-      try {
-        const result = await hf.featureExtraction({
-          model: "sentence-transformers/all-MiniLM-L6-v2",
-          inputs: text,
-        });
-        // console.log("Embedding API result:", result); // Log the entire result
-    return result
-      } catch (err) {
-        console.error("Error converting text to embeddings:", err);
-        throw err;
-      }
-    };
-    // Function to read user input
-  const promptUserInput = (query) => {
-      const rl = readline.createInterface({
+
+const promptUserInput = (query) => {
+    const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout,
-      });
-      return new Promise((resolve) =>
+    });
+    return new Promise((resolve) =>
         rl.question(query, (answer) => {
-          rl.close();
-          resolve(answer);
+        rl.close();
+        resolve(answer);
         })
-      );
-    };
-  // Function to  extract the text data from a PDF resume file asynchronously.
-  const extractJobs = (text) => {
-    const jobsPattern = /Jobs([\s\S]+?)For /i;
-    const jobsMatch = jobsPattern.exec(text);
-    if (jobsMatch) {
-      return jobsMatch[1].split(/[^a-zA-Z0-9]+/).map(item => item.trim().toLowerCase()).filter(item => item.length > 0);
-    }
-    return [];
-  };
-    
-  // Function to store embeddings in Chroma DB
-  const storeEmbeddingsInChromaDB = async (jobPostings) => {
-    const jobEmbeddings = [];
-    const metadatas = jobPostings.map(() => ({})); // Empty metadata objects
-    for (const job of jobPostings) {
-      const embedding = await generateEmbeddings(job.jobDescription.toLowerCase());
-       jobEmbeddings.push(embedding);
-    }
+    );
+};
+
+const storeEmbedding = async (jobPostings) => {
+    const metadatas = jobPostings.map(() => ({}));
+
+    const descriptions = jobPostings.map((job) => job.jobDescription.toLowerCase());
+
+    const embeddings = await generateEmbeddings(descriptions);
+
     const ids = jobPostings.map((_, index) => index.toString());
-    const jobTexts = jobPostings.map(job => job.jobTitle);
-  
+    const documents = jobPostings.map(job => job.jobTitle);
+
     try {
-      const collection = await chroma.getOrCreateCollection({ name: collectionName });
-      
-      await collection.add({
-        ids: ids,
-        documents: jobTexts,
-        embeddings: jobEmbeddings,
-        metadatas: metadatas,
-      });
-      console.log("Stored embeddings in Chroma DB.");
+        const collection = await chroma.getOrCreateCollection({ name: collectionName });
+        
+        await collection.add({
+            ids,
+            documents,
+            embeddings,
+            metadatas,
+        });
+
+        console.log("Stored embeddings in Chroma DB.");
     } catch (error) {
-      console.error("Error storing embeddings in Chroma DB:", error);
-      throw error;
+        console.error("Error storing embeddings in Chroma DB:", error);
+        throw error;
     }
-  };
-  // Main function to run the extraction, embedding generation and job recommendation process
-  const main = async () => {
-      try {
-        await storeEmbeddingsInChromaDB(smartRecommendationSystem);
-    
-        // Extract and process the recipe PDF
-        const filePath = await promptUserInput("Enter the path to the resume PDF: ");
+};
+
+const main = async () => {
+
+    try {
+        await storeEmbedding(jobPostings);
+      
+        const filePath = await promptUserInput("Enter the path to the PDF: ");
+
         const text = await extractTextFromPDF(filePath);
-        const jobs = extractJobs(text);
-    
-        if (jobs.length > 0) {
-          console.log("Extracted Jobs:", jobs);
-    
-          // Generate embedding for the extracted ingredients
-          const resumeEmbedding = await generateEmbeddings(jobs.join(' ').toLowerCase());
-    
-          // Query Chroma DB for similar recipes
-          const collection = await chroma.getCollection({ name: collectionName });
-          const results = await collection.query({
-            queryEmbeddings: [jobEmbedding],
-            n: 5, // Get top 5 similar items
-          });
-    
-          console.log("Chroma DB Query Results:", results);
-    
-          if (results.ids.length > 0 && results.ids[0].length > 0) {
-            console.log("Recommended Jobs:");
-            results.ids[0].forEach((id, index) => {
-              const recommendedItem = jobItems[parseInt(id)];
-              console.log(`Top ${index + 1} Recommended Item ==> ${recommendedItem.job_name}`);
+        const embeding = await generateEmbeddings(text.toLowerCase());
+
+        const collection = await chroma.getCollection({ name: collectionName });
+
+        const results = await collection.query({
+            queryEmbeddings: [embeding],
+            n: 3,
+        });
+
+        if (results.ids.length > 0 && results.ids[0].length > 0) {
+            const first3Items = results.ids[0].slice(0, 3);
+
+            first3Items.forEach((id, index) => {
+            const recommendedItem = jobPostings[parseInt(id)];
+
+            console.log(`Top ${index + 1} Recommended ==> ${recommendedItem.jobTitle}`);
             });
-          } else {
-            console.log("No similar jobs found.");
-          }
+        
         } else {
-          console.log("No jobs found.");
+            console.log("No similar items found.");
         }
-      } catch (err) {
+
+    } catch (err) {
         console.error("An error occurred:", err);
-      }
-    };
-    
-  // Run the main function
-  main();
+    }
+};
+
+main();
